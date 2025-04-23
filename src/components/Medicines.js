ded8009368e3bc344e1144ca815b86747ad13962
Luchazesgit <luchazesdonascimento@gmail.com>
2025-04-23 14:48:26 +0100

    feat: Adicionando gerador de pdf na conta de user

--- src/components/Medicines.js
+++ src/components/Medicines.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+
 
 export default function Medicines() {
   const [paciente, setPaciente] = useState(null);
+  const [medico, setMedico] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -18,7 +22,8 @@ export default function Medicines() {
         .from("pacientes")
         .select("*")
         .eq("id", usarioLogado.id)
-        .single();
+        .single()
+
 
       if (error) {
         console.error("Erro ao buscar dados do paciente:", error);
@@ -31,6 +36,83 @@ export default function Medicines() {
 
     fetchPaciente();
   }, []);
+
+  const gerarPDF = async (paciente) => {
+
+    const { data, error } = await supabase
+    .from("usuarios")
+    .select("*")
+    .eq("id", paciente.medico_id)
+    .single();
+
+    if (error) {
+      console.error("Erro ao buscar dados do medico:", error);
+    } else {
+      setMedico(data);
+    }
+
+      const doc = new jsPDF({
+        orientation: "portrait",
+        unit: "mm",
+        format: "a4",
+      });
+  
+      const larguraPagina = doc.internal.pageSize.getWidth();
+  
+      // Título
+      doc.setFont("helvetica", "bold");
+      doc.setFontSize(14);
+      doc.text("RELATÓRIO DO PACIENTE", larguraPagina / 2, 40, { align: "center" });
+  
+      // Tabela com dados do paciente
+      doc.setTextColor("#000");
+      doc.setFont("helvetica", "normal");
+      autoTable(doc, {
+        startY: 50,
+        styles: {
+          font: "helvetica",
+          fontSize: 12,
+          halign: "center",
+        },
+        headStyles: {
+          fillColor: "#f3e5f5",
+          textColor: "#6A1B9A",
+          fontStyle: "bold",
+        },
+        columnStyles: {
+          0: { halign: "left" },
+          1: { halign: "right" },
+        },
+        body: [
+          ["Paciente", paciente.nome],
+          ["Histórico Médico", paciente.historico || "—"],
+          ["Idade", paciente.idade || "—"],
+          ["Data", paciente.data_desconexao || "—"],
+          ["Hora", paciente.hora_desconexao || "—"],
+        ],
+        theme: "grid",
+        margin: { left: 30, right: 30 },
+      });
+  
+      // Texto de limitação funcional
+      doc.setFont("helvetica", "bold");
+      doc.setFontSize(14);
+      doc.text("A limitação funcional é de:", larguraPagina / 2, doc.lastAutoTable.finalY + 20, { align: "center" });
+  
+      doc.setFontSize(20);
+      doc.text(`${paciente.percentual ?? 0}º`, larguraPagina / 2, doc.lastAutoTable.finalY + 33, { align: "center" });
+  
+      // Assinatura
+      doc.setFont("helvetica", "normal");
+      doc.setTextColor("#000");
+      doc.setFontSize(12);
+      doc.text("Atendido por:", larguraPagina / 2, doc.lastAutoTable.finalY + 60, { align: "center" });
+  
+      doc.setFont("helvetica", "bold");
+      doc.text(data.nome || "Médico não identificado", larguraPagina / 2, doc.lastAutoTable.finalY + 68, { align: "center" });
+  
+      doc.save(`Relatorio_${paciente.nome}.pdf`);
+    };
 
   if (loading) {
     return <p className="text-center mt-4">Carregando informações...</p>;
@@ -55,13 +137,20 @@ export default function Medicines() {
           <h5 className="mb-0 text-white" >Dados recolhidos</h5>
         </div>
         <div className="card-body" style={{ paddingTop: "20px" }}>
+          <button
+                className="btn btn-sm btn-primary"
+                style={{float: "right"}}
+                onClick={() => gerarPDF(paciente)}
+              >
+              Baixar PDF
+          </button>
           <p><strong>Nome:</strong> {paciente.nome}</p>
           <p><strong>Telefone:</strong> {paciente.telefone || "Não informado"}</p>
           <p><strong>Idade:</strong> {paciente.idade} anos</p>
           <p><strong>Sexo:</strong> {paciente.sexo === "male" ? "Masculino" : paciente.sexo === "female" ? "Feminino" : "Não informado"}</p>
           <p><strong>Histórico:</strong> {paciente.historico || "Não informado"}</p>
           <p><strong>Email:</strong> {paciente.email || "Não informado"}</p>
-          <p><strong>Percentual:</strong> {paciente.percentual}°</p>
+          <p><strong>Ângulo:</strong> {paciente.percentual}°</p>
           <p><strong>Data de medição:</strong> {paciente.data_desconexao || "Não informada"}</p>
           <p><strong>Hora de medição:</strong> {paciente.hora_desconexao || "Não informada"}</p>
         </div>
